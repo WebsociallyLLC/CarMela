e804eace8780c73fb62ce0293c6005321aa00e20
Talha Tariq <talhacoder01@gmail.com>
2025-06-06 01:12:34 +0500

    done'

--- src/commonPages/ContactUs/V1/constant.ts
+++ src/commonPages/ContactUs/V1/constant.ts
@@ -1,5 +1,5 @@
 export const contactData = {
   address: '3950 227th Ave NW SAINT FRANCIS MN 55070',
   email: 'takeoffmotorcars@gmail.com',
-  phone: '(763) 442-2767',
+  phone: '(763) 373-8434',
 };
